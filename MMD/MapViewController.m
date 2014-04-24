f8b8f1fd92d630aab12029935700b9ca6c2ffa29
simeusm <simeusm@gmail.com>
2014-04-24 13:20:35 -0500

    New

--- MMD/MapViewController.m
+++ MMD/MapViewController.m
@@ -19,6 +19,7 @@
 {
     CLLocationManager *localManager;
     CLLocation *currentLocation;
+    NSArray *Name;
 }
 @property MKPointAnnotation *currentPlaceAnnotation;
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
@@ -35,18 +36,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    CLLocationCoordinate2D zoomLocation;
-    zoomLocation.latitude = 41.8819;
-    zoomLocation.latitude = -87.6278;
-    
-    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 6, 6);
-    MKCoordinateRegion adjustedRegion = [_mapView regionThatFits:viewRegion];
-    [_mapView setRegion:adjustedRegion animated:YES];
-    
-    _mapView.delegate = self;
-    
-//    self.title = @"Home";
     
     //Set the gesture
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
@@ -83,13 +72,26 @@
     
     [self.mapView setUserInteractionEnabled:YES];
     [self.mapView setUserTrackingMode:MKUserTrackingModeFollow];
-//    
-//    _sidebarButton.target = self.revealViewController;
-//    _sidebarButton.action = @selector(revealToggle:);
-//    _sidebarButton.tintColor = [UIColor colorWithWhite:0.96f alpha:0.2f];
+   
+
     
     // Add pan gesture to hide the sidebar
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    
+    
+    [PFGeoPoint geoPointForCurrentLocationInBackground:^(PFGeoPoint *geoPoint, NSError *error) {
+        if (!error) {
+            [[PFUser currentUser] setObject:geoPoint forKey:@"current location"];
+            [[PFUser currentUser] saveInBackground];
+            
+            PFQuery *User = [PFUser query];
+            [User selectKeys:@[@"username"]];
+            [User findObjectsInBackgroundWithBlock:^(NSArray *results, NSError *error) {
+                Name = results;
+            }];
+        }
+    }];
     
     
     MKMapView * map = [[MKMapView alloc] initWithFrame:self.view.bounds];
@@ -155,70 +157,10 @@
      }];
 }
 
-//- (IBAction)parkButtonPressed:(id)sender
-//{
-//    //Search Request for Parks
-//    MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
-//    request.naturalLanguageQuery = @"parks";
-//    request.region = _mapView.region;
-//    
-//    MKLocalSearch *localSearch = [[MKLocalSearch alloc] initWithRequest:request];
-//    [localSearch startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
-//        
-//        NSMutableArray *annotations = [NSMutableArray array];
-//        
-//        [response.mapItems enumerateObjectsUsingBlock:^(MKMapItem *item, NSUInteger idx, BOOL *stop) {
-//            
-//            NSLog(@"%@",request);
-//            
-//            
-//            [annotations addObject:annotations];
-//        }];
-//        
-//        [self.mapView addAnnotations:annotations];
-//    }];
-
-    
-    //    CLGeocoder *geocoder = [CLGeocoder new];
-    //    [geocoder geocodeAddressString:@"dog parks" completionHandler:^(NSArray *placemarks, NSError *error) {
-    //        for (CLPlacemark *place in placemarks) {
-    //            MKPointAnnotation *annotation = [MKPointAnnotation new];
-    //            annotation.coordinate = place.location.coordinate;
-    //            [self.mapView addAnnotation:self.currentPlaceAnnotation];
-    //        }
-    //    }];
-    
-//}
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:NO];
 }
-
-//- (IBAction)logOut:(id)sender
-//{
-//    [PFUser logOut];
-//    [self performSegueWithIdentifier:@"showLogin" sender:self];
-//}
-
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-//{
-//    if ([segue.identifier isEqualToString:@"showLogin"])
-//    {
-//        [segue.destinationViewController setHidesBottomBarWhenPushed:YES];
-//    }
-//}
-
-//- (IBAction)unwindToMapViewController:(UIStoryboardSegue *)sender
-//{
-//    BreedViewController *source;
-//    source = [sender sourceViewController];
-//}
-
-//- (IBAction)unwindToMapView:(UIStoryboardSegue *)sender
-//{
-//    BreedViewController *source;
-//    source = [sender sourceViewController];
-//}
 
 @end
