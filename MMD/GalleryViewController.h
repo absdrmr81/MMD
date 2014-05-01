96d4d68cd9497b06ccb9b638360a89fd282f6c4d
Abel E <absdrmr81@yahoo.com>
2014-04-30 21:40:48 -0500

    added gallery VC

---
+++ MMD/GalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GalleryViewController.h
+//  MMD
+//
+//  Created by Drmrboy on 4/30/14.
+//  Copyright (c) 2014 Marlon Simeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GalleryViewController : UIViewController
+
+@end
---
+++ MMD/GalleryViewController.m
@@ -0,0 +1,66 @@
+//
+//  GalleryViewController.m
+//  MMD
+//
+//  Created by Drmrboy on 4/30/14.
+//  Copyright (c) 2014 Marlon Simeus. All rights reserved.
+//
+
+#import "GalleryViewController.h"
+
+@interface GalleryViewController () <UIScrollViewDelegate>
+
+{
+    
+    UIImageView *imageView;
+    NSArray *imageViews;
+    IBOutlet UIScrollView *myScrollView;
+}
+@end
+
+@implementation GalleryViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithRed:255/255.0f green:252/255.0f blue:237/255.0f alpha:1.0f];
+
+    CGFloat width = 0.0f;
+    
+    imageViews = @[[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"milkyWay.png"]],
+                   [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"number2.jpg"]]];
+    for (UIImageView *_imageView in imageViews)
+    {
+        [myScrollView addSubview:_imageView];
+        _imageView.frame = CGRectMake(width, 0, self.view.frame.size.width, self.view.frame.size.height);
+        _imageView.contentMode = UIViewContentModeScaleAspectFit;
+        //aggregating widths
+        width += _imageView.frame.size.width;
+    }
+    
+    
+    myScrollView.contentSize = CGSizeMake(width, myScrollView.frame.size.height);
+    //instantieated imageVIew with an image
+    //imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"milkyWay.png"]];
+    //[myScrollView addSubview:imageView];
+    //setting scrollview's content size (CGsize) to imageView size (enables scrolling around to view whole image
+    //myScrollView.contentSize = imageView.frame.size;
+    
+    //setting min/max scrollsize
+    myScrollView.minimumZoomScale = 1.0;
+    myScrollView.maximumZoomScale = 6.0;
+    
+    //setting up scrollview delegate
+    myScrollView.delegate = self;
+}
+
+//returning the view we intend to zoom (doing this is case of multiple views (scrollview need to know specifically which view to scroll)
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return imageView;
+}
+
+@end
