a098cd0310aeebeae795933db5e13973bfcae05f
Abel E <absdrmr81@yahoo.com>
2014-04-30 19:53:02 -0500

    removed and re-added some images plus configured the profile

--- MMD/ProfileViewController.m
+++ MMD/ProfileViewController.m
@@ -12,11 +12,14 @@
 #import "CalculateAgeViewController.h"
 #import "BreedViewController.h"
 #import <Parse/Parse.h>
+#import <QuartzCore/QuartzCore.h>
 
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (strong, nonatomic) PFUser *currentUser;
+@property (strong, nonatomic) IBOutlet UIImageView *profilePicImageView;
+@property (strong, nonatomic) IBOutlet UIButton *profilePicButton;
 
 @end
 
@@ -30,6 +33,7 @@
     UIBarButtonItem *flipButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"btn_nav_std"] style:UIBarButtonItemStyleBordered target:self.revealViewController action:@selector(revealToggle:)];
     //Setting it to left-side of Navi bar
     self.navigationItem.leftBarButtonItem = flipButton;
+    
     self.view.backgroundColor = [UIColor colorWithRed:255/255.0f green:252/255.0f blue:237/255.0f alpha:1.0f];
     
     self.navigationController.navigationBar.backgroundColor = [UIColor colorWithRed:76/255.0f green:76/255.0f blue:66/255.0f alpha:1.0f];
@@ -41,16 +45,24 @@
     //Set the gesture
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     
-    PFUser *currentUser = [PFUser currentUser];    
+    self.currentUser = [PFUser currentUser];
 
-    if (currentUser)
+    if (self.currentUser)
     {
-        NSLog(@"Current user: %@", currentUser.username);
+        NSLog(@"Current user: %@", self.currentUser.username);
     }
     else
     {
         [self performSegueWithIdentifier:@"showLogin" sender:self];
     }
+    
+    
+    CALayer *imageLayer = self.profilePicImageView.layer;
+    [imageLayer setCornerRadius:51];
+    [imageLayer setBorderWidth:1];
+    [imageLayer setBorderColor:[UIColor colorWithRed:124/255.0f green:140/255.0f blue:48/255.0f alpha:1.0f].CGColor];
+    [imageLayer setMasksToBounds:YES];
+
 }
 
 -(void)viewWillAppear:(BOOL)animated
@@ -59,7 +71,66 @@
     self.dogNameLabel.text = self.currentUser[@"dogName"];
     self.dogBreedLabel.text = self.currentUser[@"dogBreed"];
     self.dogAgeLabel.text = [self.currentUser[@"dogAge"] description];
+    
+    if (self.currentUser[@"avatar"])
+    {
+        [self.currentUser[@"avatar"] getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            if (!error) {
+                UIImage *photo = [UIImage imageWithData:data];
+                self.profilePicImageView.image = photo;
+            }
+        }];
+    }
+    else
+    {
+        self.profilePicImageView.image = [UIImage imageNamed:@"ic_profile_pressed"];
+    }
+    self.profilePicImageView.clipsToBounds = YES;
+    
+    [self.currentUser save];
 }
+
+- (IBAction)profilePicButtonPressed:(id)sender
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+	picker.delegate = self;
+    
+	if((UIButton *) sender == self.profilePicButton)
+    {
+        picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+	}
+    else
+    {
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+	}
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [[picker presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+    
+    // saving a uiimage to pffile
+    UIImage *pickedImage = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    NSData* data = UIImageJPEGRepresentation(pickedImage,1.0f);
+    PFFile *imageFile = [PFFile fileWithData:data];
+    PFUser *user = [PFUser currentUser];
+    user[@"avatar"] = imageFile;
+    
+    // getting a uiimage from pffile
+    [self.currentUser[@"avatar"] getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        if (!error) {
+            UIImage *photo = [UIImage imageWithData:data];
+            self.profilePicImageView.image = photo;
+        }
+    }];
+    
+    [user save];
+}
+
+
 
 - (IBAction)logOut:(id)sender
 {
